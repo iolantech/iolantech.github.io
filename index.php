f8a63868ea8775cbf7849d7177024d4c0d223c52
IolanTech <iolantech@gmail.com>
2020-06-04 23:51:32 -0300

    Adicionando boilerplate do projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Beginner</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/spacelab/bootstrap.css">
+</head>
+<body>
+
+<h2>Olá Mundo! Inicializando app!</h2>
+
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
